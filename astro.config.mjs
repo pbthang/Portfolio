60fa9446dc8546497900672befd26c7ba2b5b13e
Thang Pham <bathang02@gmail.com>
2023-04-05 16:03:40 +0800

    fix deployment error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,10 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 import robotsTxt from "astro-robots-txt";
+import netlify from "@astrojs/netlify/edge-functions";
 
-// https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), robotsTxt()],
   site: "https://pbthang.netlify.app",

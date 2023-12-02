037870a0df920662e96f43e622d3c00029b92ce7
Thang Pham <bathang02@gmail.com>
2023-12-02 23:15:58 +0800

    update styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,23 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["business"],
+    themes: [
+      {
+        mytheme: {
+          primary: "#14213d",
+          "primary-content": "#f0f3f6",
+          secondary: "#fca311",
+          accent: "#fca311",
+          neutral: "#212529",
+          "base-100": "#f0f3f6",
+          info: "#ffffff",
+          success: "#12b886",
+          warning: "#f57c00",
+          error: "#fa5252",
+          "--rounded-btn": "6px",
+          "--rounded-box": "6px",
+        },
+      },
+    ],
   },
 };
